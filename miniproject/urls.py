24accdd77f76effcc0236d98ba5b7a418d80d06b
NikolayB7 <biletskiyna@gmail.com>
2022-03-06 20:33:52 +0200

    migrations

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +25,7 @@ urlpatterns = [
     path("", include("news.urls")),
     path("blog/", index),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
