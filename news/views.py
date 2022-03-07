1a8a0c68d0cc0bf0138f2f3b402268f6f198760b
NikolayB7 <biletskiyna@gmail.com>
2022-03-07 16:54:24 +0200

    template

--- news/views.py
+++ news/views.py
@@ -1,16 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import News
+
+
+# def index(request):
+#     news = News.objects.all()
+#     res = "<h3>Список новостей</h3>"
+#     for item in news:
+#         res += f"<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n"
+#     return HttpResponse(res)
 
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse("<h3>Page news</h3>")
-
-
-def test(request):
-    # print(dir(request))
-    return HttpResponse("<h1>Тестовая страница</h1>")
+    news = News.objects.order_by("-creates_at")
+    context = {"news": news, "title": "Список новостей"}
+    return render(request, "news/index.html", context)
 
 
 # Create your views here.
